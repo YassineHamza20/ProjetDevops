85c4a75779965ffea98f87da51186de788e7cfd0
Yassine Hamza <yassine.hamza@esprit.tn>
2024-04-16 20:29:20 +0100

    b

--- src/test/java/tn/esprit/rh/achat/service/operateur/OperateurServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/service/operateur/OperateurServiceImplTest.java
@@ -1,46 +1,48 @@
 package tn.esprit.rh.achat.service.operateur;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import org.junit.runner.RunWith;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
+import tn.esprit.rh.achat.controllers.OperateurController;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import tn.esprit.rh.achat.controllers.OperateurController;
-import org.springframework.test.web.servlet.MockMvc;
 
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-@RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
 public class OperateurServiceImplTest {
     @Mock
     private OperateurRepository operateurRepositoryMock;
-    @InjectMocks
-    private OperateurServiceImpl operateurService = new OperateurServiceImpl();
-    @InjectMocks
-    private OperateurController operateurController = new OperateurController();
-    MockMvc mockMvc = MockMvcBuilders.standaloneSetup(operateurController).build();
 
+    @InjectMocks
+    private OperateurServiceImpl operateurService;
+
+    @InjectMocks
+    private OperateurController operateurController;
+
+    private MockMvc mockMvc;
+
+    public OperateurServiceImplTest() {
+        mockMvc = MockMvcBuilders.standaloneSetup(operateurController).build();
+    }
 
     @Test
     public void testRetrieveAllOperators() {
         List<Operateur> expectedOperators = Arrays.asList(
-                new Operateur(1L, "Operator A","A","yassine", new HashSet<>()),
-                new Operateur(2L, "Operator B","B","yassine", new HashSet<>())
+                new Operateur(1L, "Operator A", "A", "yassine", new HashSet<>()),
+                new Operateur(2L, "Operator B", "B", "yassine", new HashSet<>())
         );
 
         when(operateurRepositoryMock.findAll()).thenReturn(expectedOperators);
@@ -50,11 +52,10 @@ public class OperateurServiceImplTest {
         assertEquals(expectedOperators, actualOperators);
     }
 
-
     @Test
     void addOperator() {
         Operateur newOperateur = new Operateur(1L, "operator 1", "yass", "yassine", new HashSet<>());
-        when(operateurService.addOperateur(newOperateur)).thenReturn(new Operateur());
+        when(operateurRepositoryMock.save(any(Operateur.class))).thenReturn(newOperateur);
 
         Operateur addedOperator = operateurController.addOperateur(newOperateur);
         assertEquals(newOperateur, addedOperator);
@@ -62,19 +63,17 @@ public class OperateurServiceImplTest {
 
     @Test
     void removeOperateur() throws Exception {
-        Long OperateurId = 1L;
-
-        mockMvc.perform(delete("/operatot/{operatorId}", OperateurId))
+        Long operateurId = 1L;
+        mockMvc.perform(delete("/operateur/remove-operateur/{operateur-id}", operateurId))
                 .andExpect(status().isOk());
     }
 
     @Test
     void modifyOperateur() {
-        Operateur modifiedOperateur = new Operateur(1L,"operator 1" ,"yass" ,"yassine",new HashSet<>());
-        when(operateurService.updateOperateur(modifiedOperateur)).thenReturn(modifiedOperateur);
-        Operateur updatedOperator  = operateurController.modifyOperateur(modifiedOperateur);
+        Operateur modifiedOperateur = new Operateur(1L, "operator 1", "yass", "yassine", new HashSet<>());
+        when(operateurRepositoryMock.save(any(Operateur.class))).thenReturn(modifiedOperateur);
+
+        Operateur updatedOperator = operateurController.modifyOperateur(modifiedOperateur);
         assertEquals(modifiedOperateur, updatedOperator);
     }
-
-
 }
